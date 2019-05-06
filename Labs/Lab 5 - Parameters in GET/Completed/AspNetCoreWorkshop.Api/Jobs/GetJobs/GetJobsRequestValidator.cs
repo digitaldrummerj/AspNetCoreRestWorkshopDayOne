1998e62a76b03b6bc7bc8f800e0c4982c7f82003
Justin James <digitaldrummerj@gmail.com>
2019-05-06 14:26:44 -0700

    add: completed Lab 5 Solution

---
+++ Labs/Lab 5 - Parameters in GET/Completed/AspNetCoreWorkshop.Api/Jobs/GetJobs/GetJobsRequestValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using System;
+using System.Linq;
+
+namespace AspNetCoreWorkshop.Api.Jobs.GetJobs
+{
+    public class GetJobsRequestValidator : AbstractValidator<GetJobsRequest>
+    {
+        public GetJobsRequestValidator()
+        {
+            RuleFor(r => r.OrderBy).Must(BeAValidProperyName)
+            .WithMessage("OrderBy parameter not valid.");
+        }
+
+        public bool BeAValidProperyName(string propName)
+        {
+            if (string.IsNullOrWhiteSpace(propName))
+            {
+                return true;
+            }
+
+            var validPropNames = typeof(GetJobsResponse)
+                .GetProperties()
+                .Select(p => p.Name);
+
+            return validPropNames.Contains(propName);
+        }
+    }
+}
